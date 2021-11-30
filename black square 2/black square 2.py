aaba1352026ec1e724730522649f80af4fd122eb
bynik1 <alesha.bessonov2003@mail.ru>
2021-11-30 18:34:44 +0700

    Менял название файлов и добавил решение таксков

--- black square 2/black square 2.py
+++ black square 2/black square 2.py
@@ -1,9 +1,9 @@
 from stegano import exifHeader
 
-#secret = exifHeader.hide("4.jpg", "4_secret.jpg", "Битва негров ночью")
+#secret = exifHeader.hide("black square 2.jpg", "modern Malevich square 2.jpg", "Битва негров ночью")
 #Шифруем в картинку 4.jpg сообщение: Битва Негров ночью; зашифрованный файл с названием 4_secret.jpg
 
-result = exifHeader.reveal("4_secret.jpg")
+result = exifHeader.reveal("modern Malevich square 2.jpg")
 result = result.decode()
 #декодирование текста из изображения 1
 print(result)
