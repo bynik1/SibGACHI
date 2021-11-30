8973689d291a64589857295acb2a9b6af71d6650
bynik1 <alesha.bessonov2003@mail.ru>
2021-11-30 14:32:30 +0700

    Задания

---
+++ black square 2/black square 2.py
@@ -0,0 +1,9 @@
+from stegano import exifHeader
+
+#secret = exifHeader.hide("4.jpg", "4_secret.jpg", "Битва негров ночью")
+#Шифруем в картинку 4.jpg сообщение: Битва Негров ночью; зашифрованный файл с названием 4_secret.jpg
+
+result = exifHeader.reveal("4_secret.jpg")
+result = result.decode()
+#декодирование текста из изображения
+print(result)
